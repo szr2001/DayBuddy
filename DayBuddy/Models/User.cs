8adcd42158c7a4a09949c85f2b45fcd91299c418
Szabo Robert <szr2001@yahoo.com>
2024-11-18 15:08:40 +0200

    Made the Login and Register view pretty

--- DayBuddy/Models/User.cs
+++ DayBuddy/Models/User.cs
@@ -5,12 +5,17 @@ namespace DayBuddy.Models
     public class User
     {
         [Required]
+        [MaxLength(20, ErrorMessage ="Name too long")]
         public string Name { get; set; }
 
         [Required]
         [EmailAddress(ErrorMessage = "Invalid Email")]
+        [MaxLength(80, ErrorMessage ="Email too long")]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
+        [Required]
+        [Display(Name ="Repeat Password")]
+        public string RepeatPassword { get; set; }
     }
 }
