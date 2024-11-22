e23398acd428ab08ca5dd08fc0d346c40f092501
Szabo Robert <szr2001@yahoo.com>
2024-11-22 11:24:40 +0200

    Added Model for User Meesages and chat Lobby

---
+++ DayBuddy/Models/BuddyChatLobby.cs
@@ -0,0 +1,17 @@
+﻿using MongoDbGenericRepository.Attributes;
+using System.Configuration;
+
+namespace DayBuddy.Models
+{
+    [CollectionName("ActiveChats")]
+    public class BuddyChatLobby
+    {
+        public string Id { get; set; }
+        public string[] Users { get; set; }
+        public BuddyChatLobby(string[] users)
+        {
+            Id = Guid.NewGuid().ToString();
+            Users = users;
+        }
+    }
+}
---
+++ DayBuddy/Models/BuddyMessage.cs
@@ -0,0 +1,19 @@
+﻿using MongoDbGenericRepository.Attributes;
+
+namespace DayBuddy.Models
+{
+    [CollectionName("Messages")]
+    public class BuddyMessage
+    {
+        public string Message { get; set; }
+        public string UserId { get; set; }
+        public string ChatLobbyId { get; set; }
+        
+        public BuddyMessage(string message, string userId, string chatLobbyId)
+        {
+            Message = message;
+            UserId = userId;
+            ChatLobbyId = chatLobbyId;
+        }
+    }
+}
