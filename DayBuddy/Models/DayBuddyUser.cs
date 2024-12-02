aa33923a967a3659ef276dc69e96dffba13d7ee6
Szabo Robert <szr2001@yahoo.com>
2024-12-02 21:07:51 +0200

    Set the default values on users to empty

--- DayBuddy/Models/DayBuddyUser.cs
+++ DayBuddy/Models/DayBuddyUser.cs
@@ -10,12 +10,12 @@ namespace DayBuddy.Models
     {
         public int Age { get; set; } = 18;
         public int[] AgeRange { get; set; } = { 18, 100 }; 
-        public string[] Interests { get; set; } = ["Programming", "Movies", "Sleeping", "Reading", "Dogs"];
-        public string? Gender { get; set; } = "None";
+        public string[] Interests { get; set; } = [];
+        public string? Gender { get; set; } = "";
         public float Score { get; set; } = 8;
-        public string? Sexuality { get; set; } = "None";
-        public string? Country { get; set; } = "None";
-        public string? City { get; set; } = "None";
+        public string? Sexuality { get; set; } = "";
+        public string? Country { get; set; } = "";
+        public string? City { get; set; } = "";
         public DateTime? LastTimeOnline { get; set; }
         public DateTime? PurchasedPremium { get; set; }
         public DateTime? MatchedWithBuddy { get; set; }
