91d715f5d43ea00abbcc0cabf642762ebed827c7
Szabo Robert <szr2001@yahoo.com>
2024-12-20 18:32:45 +0200

    Added delete account button enable delay

---
+++ DayBuddy/wwwroot/js/DeleteAccount.js
@@ -0,0 +1,9 @@
+﻿var $deleteBtn = $("#DeleteBtn");
+
+$(document).ready(function () {
+    setTimeout(EnableButton,3000);
+})
+
+function EnableButton() {
+    $deleteBtn.attr("disabled", false);
+}
