36c0099c6c8ccc0f6d1d1082aba068171b90685f
Szabo Robert <szr2001@yahoo.com>
2025-01-18 15:28:00 +0200

    Reset my email password, extracted the hard coded email values into appsettings

--- DayBuddy/Services/GmailSMTPEmailService.cs
+++ DayBuddy/Services/GmailSMTPEmailService.cs
@@ -5,15 +5,26 @@ namespace DayBuddy.Services
 {
     public class GmailSMTPEmailService
     {
-        private readonly string gmailPassword = "lqbkzqsasnzjbaqo";
-        private readonly string gmail = "daybuddy2024@gmail.com";
+        private readonly IConfiguration config;
+        private string emailPassword = "";
+        private string emailAdress = "";
+        public GmailSMTPEmailService(IConfiguration config)
+        {
+            this.config = config;
+            emailAdress = config.GetValue<string>("EmailAdress")!;
+            emailPassword = config.GetValue<string>("EmailPassword")!;
+        }
+
         public async Task<bool> TrySendEmailAsync(string targetEmail, string subject, string htmlContent)
         {
+            //handle testing enviroment
+            if (string.IsNullOrEmpty(emailPassword) || string.IsNullOrEmpty(emailAdress)) return true;
+
             try
             {
                 MailMessage message = new();
 
-                message.From = new MailAddress(gmail);
+                message.From = new MailAddress(emailAdress);
                 message.Subject = subject;
 
                 message.To.Add(new MailAddress(targetEmail));
@@ -23,7 +34,7 @@ namespace DayBuddy.Services
                 SmtpClient smtpClient = new("smtp.gmail.com")
                 {
                     Port = 587,
-                    Credentials = new NetworkCredential(gmail, gmailPassword),
+                    Credentials = new NetworkCredential(emailAdress, emailPassword),
                     EnableSsl = true
                 };
 
